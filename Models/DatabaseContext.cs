8e58b66bc2cc7778157fa33e0bddc4b41eac6d31
HalimHamidov <halimjonik@gmail.com>
2020-11-20 15:35:38 +0300

    feat: add HW3

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication12.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Files> Files { get; set; }
+    }
+
+}
